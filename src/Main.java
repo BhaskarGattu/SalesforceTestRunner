e3f14b1ce61829e3b69f77923797b0b7b67efcdb
z003858 <Menaka.Kothandan@rntbci.com>
2017-06-22 17:14:05 +0530

    asd

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,7 @@ import com.sforce.ws.ConnectorConfig;
  * Date: 13/08/12
  * Time: 5:03 PM
  * To change this template use File | Settings | File Templates.
- * 
- *  */
+ *  *  */
 
 public class Main {
     
